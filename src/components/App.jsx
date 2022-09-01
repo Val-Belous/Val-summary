afd14eb81071bec4ace8906e69b5e7ada40fe410
Val-Belous <vaaalbelous@gmail.com>
2022-09-02 01:49:17 +0300

    cube done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1 className="maintext">Val Belous Summary</h1>
+      <div>
+        <div className="box">
+          <div className="box__face box__face--back">Back</div>
+          <div className="box__face box__face--front">Front</div>
+          <div className="box__face box__face--left">Left</div>
+          <div className="box__face box__face--right">Right</div>
+          <div className="box__face box__face--top">Top</div>
+          <div className="box__face box__face--bottom">Bottom</div>
+        </div>
+      </div>
+    </>
   );
 };
