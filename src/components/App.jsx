5f8e95674f3a8b7cf626d17afd824417f3aadf54
Val-Belous <vaaalbelous@gmail.com>
2022-09-03 01:14:23 +0300

    change cube

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,11 +43,11 @@ export const App = () => {
   return (
     <div className="container">
       <h1 className="maintext">Welcome!</h1>
-      <div>
+      <div className="scene">
         <div className="box" id="box" ref={ref}>
-          <FrontAboutMe />
+          {/* <FrontAboutMe /> */}
           <div className="box__face box__face--back">Back</div>
-          {/* <div className="box__face box__face--front">Front</div> */}
+          <div className="box__face box__face--front">Front</div>
           <div className="box__face box__face--left">Left</div>
           <div className="box__face box__face--right">Right</div>
           <div className="box__face box__face--top">Top</div>
