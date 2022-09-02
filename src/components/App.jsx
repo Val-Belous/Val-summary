602a02ffb0d71ee18c90553816c072502281cb22
Val-Belous <vaaalbelous@gmail.com>
2022-09-02 20:05:01 +0300

    cube left move

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,57 @@
+import { useRef, useEffect } from 'react';
 import { FrontAboutMe } from './FrontAboutMe/FrontAboutMe';
 
 export const App = () => {
+  const ref = useRef(null);
+  useEffect(() => {
+    const onKeyPress = evt => {
+      switch (evt.keyCode) {
+        case 37:
+          console.log('lol');
+          if (element.classList.contains('toleftone')) {
+            element.classList.remove('toleftone');
+            element.classList.add('tolefttwo');
+          } else if (element.classList.contains('tolefttwo')) {
+            element.classList.remove('tolefttwo');
+            element.classList.add('toleftthree');
+          } else if (element.classList.contains('toleftthree')) {
+            element.classList.remove('toleftthree');
+            element.classList.add('toleftfour');
+          } else if (element.classList.contains('toleftfour')) {
+            element.classList.remove('toleftfour');
+            element.classList.add('toleftone');
+          } else {
+            element.classList.add('toleftone');
+          }
+          break;
+        case 38:
+          break;
+        case 39:
+          break;
+
+        default:
+          break;
+      }
+    };
+    const element = ref.current;
+    window.addEventListener('keydown', onKeyPress);
+    return () => {
+      window.removeEventListener('keydown', onKeyPress);
+    };
+  }, []);
+
   return (
     <div className="container">
-      <h1 className="maintext">Val Belous Summary</h1>
+      <h1 className="maintext">Welcome!</h1>
       <div>
-        <div className="box">
+        <div className="box" id="box" ref={ref}>
           <FrontAboutMe />
           <div className="box__face box__face--back">Back</div>
           {/* <div className="box__face box__face--front">Front</div> */}
           <div className="box__face box__face--left">Left</div>
           <div className="box__face box__face--right">Right</div>
           <div className="box__face box__face--top">Top</div>
-          <div className="box__face box__face--bottom">Bottom</div>
+          <div className="box__face box__face--bottom"></div>
         </div>
       </div>
     </div>
--- src/components/FrontAboutMe/FrontAboutMe.jsx
+++ src/components/FrontAboutMe/FrontAboutMe.jsx
@@ -4,15 +4,16 @@ import photo from '../../images/myPhoto.JPG';
 export const FrontAboutMe = () => {
   return (
     <div className={styles.container}>
+      <h2 className={styles.mainTitle}>Hello, my name is Val Belous</h2>
       <div className={styles.box}>
         <img className={styles.image} src={photo} alt="" />
         <div className={styles.textContainer}>
-          {/* <h1 className={styles.text}>Junior Front-end developer</h1> */}
+          <h3 className={styles.title}>Front-end developer</h3>
           <p className={styles.text}>
-            I am a front -end developer looking for a full - time job. Seeking
-            to use my good JavaScript and React.js skills. I want try to do my
-            best on my future position in order to achieve the maximum good
-            result in the shortest possible time.
+            I am junior front-end developer looking for a full - time job.
+            Seeking to use my good JavaScript and React.js skills. I want try to
+            do my best on my future position in order to achieve the maximum
+            good result in the shortest possible time.
           </p>
         </div>
       </div>
